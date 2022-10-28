b1de8fdfd6a48c7b56b313fe9e808e26328b1fd6
symi <symien.moore@gmail.com>
2022-10-27 20:12:31 -0400

    wip

---
+++ firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCCbpF6pT2uXjYrPv0R6iCI6hJ0cYQYTKA",
+  authDomain: "portfolio-d4a42.firebaseapp.com",
+  projectId: "portfolio-d4a42",
+  storageBucket: "portfolio-d4a42.appspot.com",
+  messagingSenderId: "1011243911013",
+  appId: "1:1011243911013:web:c8bd06fcc05fd1d647838c"
+};
+const app = initializeApp(firebaseConfig);
